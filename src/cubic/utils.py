a607eb8fe7d6514aca7b40aaaed124066d005ed6
blazaid <alber.diaz@proton.me>
2025-10-14 02:13:10 +0200

    Subo todo lo que tenía hecho

--- src/cubic/utils.py
+++ src/cubic/utils.py
@@ -4,45 +4,57 @@ from timezonefinder import TimezoneFinder
 import zoneinfo
 import datetime
 
-def solar_info(latitude, longitude, aware_datetime):
-    """Returns the solar event closest to the given timezone-aware datetime.
+class SolarInfo:
+    def __init__(self, latitude, longitude):
+        """Initializes this object.
 
-    :param latitude: Latitude of the location.
-    :param longitude: Longitude of the location.
-    :param aware_datetime: A timezone-aware datetime (any timezone).
-    :return: Closest solar event as string (dawn, sunrise, noon, sunset, dusk).
-    """
-    if aware_datetime.tzinfo is None or aware_datetime.utcoffset() is None:
-        raise ValueError("aware_datetime must be timezone-aware.")
+        :param latitude: Latitude of the location.
+        :param longitude: Longitude of the location.
+        :raise ValueError: If the timezone for the geoposition cannot be
+            determined.
+        """
+        self.latitude = latitude
+        self.longitude = longitude
 
-    # Obtenemos la zona horaria de la localización ...
-    tf = TimezoneFinder()
-    tz_name = tf.timezone_at(lat=latitude, lng=longitude)
-    if tz_name is None:
-        raise ValueError("Could not determine time zone.")
+        # Obtenemos la zona horaria de esta localización...
+        tf = TimezoneFinder()
+        tz_name = tf.timezone_at(lat=latitude, lng=longitude)
+        if tz_name is None:
+            raise ValueError("Could not determine time zone.")
+        self.local_tz = zoneinfo.ZoneInfo(tz_name)
+
+        # Y el «observador» de la misma
+        self.observer = Observer(latitude=self.latitude, longitude=self.longitude)
+        
+
+    def closest_event(self, aware_datetime):
+        """Returns the solar event closest to the given timezone-aware datetime.
     
-    local_tz = zoneinfo.ZoneInfo(tz_name)
+        :param aware_datetime: A timezone-aware datetime (any timezone).
+        :return: Closest solar event as string (dawn, sunrise, noon, sunset, dusk).
+        """
+        if aware_datetime.tzinfo is None or aware_datetime.utcoffset() is None:
+            raise ValueError("aware_datetime must be timezone-aware.")
+
+        # Convertimos ese datetime a la zona local del lugar ...
+        local_time = aware_datetime.astimezone(self.local_tz)
+        local_date = local_time.date()
     
-    # ... convertimos ese datetime a la zona local del lugar ...
-    local_time = aware_datetime.astimezone(local_tz)
-    local_date = local_time.date()
-
-    # ... calculamos los eventos solares en esos lugar y fecha ...
-    observer = Observer(latitude=latitude, longitude=longitude)
-    solar = sun(observer, date=local_date, tzinfo=local_tz)
-
-    # ... y eleginos el evento más cercano según la hora especificada
-    events = {
-        'dawn': solar['dawn'],
-        'sunrise': solar['sunrise'],
-        'noon': solar['noon'],
-        'sunset': solar['sunset'],
-        'dusk': solar['dusk']
-    }
-
-    closest_event = min(
-        events.items(),
-        key=lambda x: abs((local_time - x[1]).total_seconds())
-    )
-
-    return closest_event[0]
+        # ... calculamos los eventos solares en esos lugar y fecha ...
+        solar = sun(self.observer, date=local_date, tzinfo=self.local_tz)
+    
+        # ... y elegimos el evento más cercano según la hora especificada
+        events = {
+            'dawn': solar['dawn'],
+            'sunrise': solar['sunrise'],
+            'noon': solar['noon'],
+            'sunset': solar['sunset'],
+            'dusk': solar['dusk']
+        }
+    
+        closest_event = min(
+            events.items(),
+            key=lambda x: abs((local_time - x[1]).total_seconds())
+        )
+    
+        return closest_event[0]
